bc16a473c2df8d265f45117350cf5779eccdcf2c
kwonhyunwoo <kwonhyunwoo@likelion.org>
2017-06-01 14:54:43 +0000

    저장

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  
+  validates_uniqueness_of :nic_name, message: "같은 닉네임 존재합니다."
 end
